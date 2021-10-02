8f4da9e909d1252dc42f4e2f3268dccdf04138e6
Timothy Hull <timothyhull@gmail.com>
2021-10-02 22:42:09 +0000

    Update docstring. Refactor to support log levels as attributes, not strings.

--- namedtuple_maker/namedtuple_logger.py
+++ namedtuple_maker/namedtuple_logger.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 ''' Perform logging functions for namedtuple_maker.py
 
+    Requirements:
+        Install Logbook with pip:
+            pip install Logbook
+
+        https://pypi.org/project/Logbook/
+        https://logbook.readthedocs.io/en/stable/
+
     Usage:
-        TBD.
+        # Step 1, import the initialize_logging function into your application:
+            from namedtuple_logger import initialize_logging()
+            initialize_logging()
+
+        # Step 2, import the logbook module:
+            import logbook
+
+        # Step 3, create and name your own logbook.Logger:
+            app_log = logbook.Logger('App Log')
+
+        # Step 4, write log events to the log.
+            app_log.info('Application logging started.')
 '''
 
 # Imports
@@ -13,6 +31,7 @@ from os import path
 LOG_FILE_PATH = path.curdir
 LOG_FILE_NAME = 'namedtuple-log.log'
 LOG_FILE = path.join(LOG_FILE_PATH, LOG_FILE_NAME)
+LOG_LEVEL_DEFAULT = 'DEBUG'
 LOG_LEVELS = (
     'FATAL',
     'ERROR',
@@ -39,24 +58,32 @@ def initialize_logging(
 
     # If no log level is passed as an argument, set a standard logging level
     if log_level is None:
-        log_level = 'DEBUG'
+        log_level = getattr(
+            logbook,
+            LOG_LEVEL_DEFAULT
+        )
 
-    # If a log level is passed as an argument, format the string
+    # If a log level is passed as an argument, check validity and set level
     else:
-        log_level = log_level.upper().strip()
 
-    # Confirm log level is valid
+        # Confirm log level is valid
         if log_level not in LOG_LEVELS:
             error_message = (
                 f'The specified logging level "{log_level}" is invalid.\n'
                 f'Use one of the following values:\n'
             )
 
-            for index, evel in enumerate(LOG_LEVELS):
-                error_message += (f'{index + 1}. {log_level}\n')
+            for index, level in enumerate(LOG_LEVELS):
+                error_message += (f'{index + 1}. {level}\n')
 
             raise ValueError(
                 error_message
+            )
+
+        # Set logging level
+        log_level = getattr(
+                logbook,
+                log_level.upper().strip()
             )
 
     # Initialize logging
@@ -67,7 +94,7 @@ def initialize_logging(
 
     log_init_log = logbook.Logger('Logging Initializer')
     log_init_log.info(
-        f'Initialized {log_level} level logging.'
+        f'Initialized {logbook.get_level_name(log_level)} level logging.'
     )
     log_init_log.info(
         f'Log file path and root name is {log_file}.'
