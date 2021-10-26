ae24fb1354e7d7945ea7e28e1aa5cc7d5c6becb9
Timothy Hull <timothyhull@gmail.com>
2021-10-26 02:56:51 +0000

    Refactor for scalability Update docstrings

--- namedtuple_maker/namedtuple_utils.py
+++ namedtuple_maker/namedtuple_utils.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
-''' Gracefully exit after catching an exception.  Call the
-    graceful_exit function from within an except block that catches
-    an error for which the intent is to exit the program without
-    displaying full trace stack details.
+''' Utility module with functions for the namedtuple-maker application.
+
+    Usage:
+        See individual function docstrings for usage instructions.
+'''
+
+# Imports - Python Standard Library
+from os import _exit
+from sys import exit, stderr
+from typing import AnyStr
+
+
+def graceful_exit(
+    error_message: AnyStr = None,
+    error_object: Exception = None
+):
+    ''' Gracefully exit a program after catching an exception.  Call the
+    graceful_exit function from within a higher-level except block.
+    Instead of using the raise keyword to display a stack trace and
+    exit, graceful_exit will display a friendly message, and exit the
+    program without displaying full trace stack details.
 
     The graceful_exit function first attempts to use the sys.exit
     function to exit the application.  If sys.exit raises a
     SystemExit exception, usually as the result of running a program
     within an interactive shell (IDLE, iPython, etc.), graceful_exit
     will use the os._exit function.
+
+    Args:
+        error_message (AnyStr, optional):
+            String error message to display.
+
+        error_object (Exception):
+            Exception object from the source except block.  Write
+            the output to STDERR.
 
     Usage:
         # Step 1, import the graceful_exit() function into your
@@ -27,30 +52,10 @@
             try:
                 my_function()
             except NameError as error:
-                graceful_exit(error)
-'''
-
-# Imports - Python Standard Library
-from os import _exit
-from sys import exit, stderr
-from typing import AnyStr
-
-
-def graceful_exit(
-    error_message: AnyStr = None,
-    error_object: Exception = None
-):
-    ''' Gracefully exit after catching an exception exception.  Display
-        a friendly exception message with the shorthand for repr(), and
-        exit the application without displaying a full stack trace.
-        Optionally write a custom error message to STDERR.
-
-        Args:
-            error_message (AnyStr, optional):
-                String error message to display.
-
-            error_object (Exception):
-                Exception object from the source except block.
+                graceful_exit(
+                    error_message='An error occurred',
+                    error_object=error
+                )
 
         Returns:
             N/A.
